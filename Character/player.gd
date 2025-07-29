2b1f20800c68792b50de73d9aec0fc18fec12b9a
Mucki0505 Laptop <67168206+Mucki05052005@users.noreply.github.com>
2025-07-29 18:02:32 +0200

    Player_movement
    - Player Script
    extrem rudimentär

--- Character/player.gd
+++ Character/player.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+
+var speed = 400;
+
+func getInput(): 
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	getInput()
+	move_and_slide()
