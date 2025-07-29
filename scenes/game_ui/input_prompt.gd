8b97dd8a1a5c8e0784416fc5338a3a203ff5f4b2
JaN0h4ck <git@jan-nothacker.de>
2025-07-29 19:49:12 +0200

    input prompt scene and mapping

---
+++ scenes/game_ui/input_prompt.gd
@@ -0,0 +1,22 @@
+extends Panel
+
+@export var text: String
+@export var action_name: String
+@onready var input_icon_mapper: InputIconMapper = $InputIconMapper
+@onready var icon_display: TextureRect = $HBoxContainer/IconDisplay
+@onready var label: Label = $HBoxContainer/Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	icon_display.texture = get_icon(action_name, InputDeviceMonitor.is_gamepad)
+	label.text = text
+
+func get_icon(_name: String, _is_gamepad: bool):
+	var input_events = InputMap.action_get_events(_name)
+	if(input_events == null or input_events.size() == 0): return null
+	
+	var evt: InputEvent = input_events[1 if _is_gamepad else 0]
+	if evt == null: evt = input_events[0]
+	
+	var result = input_icon_mapper.get_icon(evt)
+	return result
