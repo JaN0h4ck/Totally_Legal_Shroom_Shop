16baaab4ffa0fc7a4a3c2bd0ea81126cc262a581
JaN0h4ck <git@jan-nothacker.de>
2025-07-29 21:45:52 +0200

    connect interact prompt to game manager

--- scenes/game_ui/game_manager.gd
+++ scenes/game_ui/game_manager.gd
@@ -1,11 +1,12 @@
+class_name GameManager
 extends Node
 
 @onready var ui_layer: Control = $"../UILayer"
 @export var interact_scene: PackedScene
 
-func show_interact_prompt():
+func show_interact_prompt(prompt: String):
 	var instance: InputPrompt = interact_scene.instantiate()
-	instance.text = "Interact"
+	instance.text = prompt
 	instance.action_name = "interact"
 	instance.set_anchors_preset(Control.PRESET_BOTTOM_RIGHT)
 	ui_layer.add_child(instance)
--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -1,22 +1,30 @@
 extends Area2D
 
 var player_is_inside: bool = false
+@export var interact_prompt: String = "Interact"
 
 signal player_interacted
+signal player_entered(prompt: String)
+signal player_left
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
+	var game_manager: GameManager =get_tree().root.get_node("GameUI/GameManager")
+	player_entered.connect(game_manager.show_interact_prompt)
+	player_left.connect(game_manager.destroy_interact_prompt)
 
 func _input(event: InputEvent) -> void:
 	if(event.is_action("interact")):
-		player_interacted.emit() 
+		player_interacted.emit(interact_prompt) 
 
 func _on_body_entered(body: Node2D):
 	if(body.is_in_group(&"player")):
+		player_entered.emit(interact_prompt)
 		player_is_inside = true
 
 func _on_body_exited(body: Node2D):
 	if(body.is_in_group(&"player")):
+		player_left.emit()
 		player_is_inside = false
