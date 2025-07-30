fc925c0c8e730adaaf5b284951c8274ea6249f99
JaN0h4ck <git@jan-nothacker.de>
2025-07-30 12:17:07 +0200

    create event bus

---
+++ scripts/event_bus.gd
@@ -0,0 +1,26 @@
+extends Node
+
+signal interact_mushroom_theke
+signal interact_customer
+signal interact_register
+signal interact_lexikon
+signal interact_lever
+signal interact_certificate
+
+func _on_interact_mushroom_theke():
+	interact_mushroom_theke.emit()
+
+func _on_interact_customer():
+	interact_customer.emit()
+
+func _on_interact_register():
+	interact_register.emit()
+
+func _on_interact_lexikon():
+	interact_lexikon.emit()
+
+func _on_interact_lever():
+	interact_lever.emit()
+
+func _on_interact_certificate():
+	interact_certificate.emit()
