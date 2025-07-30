194edeb178fe4d9d5c8b9fe078ab997aab4a4a29
MurtaghDorn <vogelmarkus03@gmail.com>
2025-07-30 17:58:48 +0200

    Added Cowbow Skin to NPC

--- Character/NPCs/npc.gd
+++ Character/NPCs/npc.gd
@@ -5,13 +5,15 @@ extends CharacterBody2D
 @export var path : Path2D
 @export var path_follow : PathFollow2D
 @export var move_speed : float = 60.0
-@export var loop : bool = false
+@export var loop_path : bool = false
 
 var last_position : Vector2
 var movement : Vector2
 
+var last_direction : String = "f"
+
 func _ready() -> void:
-	path_follow.loop = loop
+	path_follow.loop = loop_path
 	position = path_follow.global_position
 	last_position = position
 
@@ -21,9 +23,28 @@ func _physics_process(delta : float) -> void:
 	position = path_follow.global_position
 	
 	movement = position - last_position
-	if movement.length() > 0.1:
+	last_position = position
+	
+	play_animation(movement)
+
+func play_animation(movement):
+	if not movement.x == 0 or not movement.y == 0:
 		animated_sprite.play("walk")
 	else:
-		animated_sprite.stop()
+		if last_direction == "f":
+			animated_sprite.play("stand_front")
+		elif last_direction == "b":
+			animated_sprite.play("stand_back")
+		elif last_direction == "r":
+			animated_sprite.play("stand_right")
+		elif last_direction == "l":
+			animated_sprite.play("stand_left")
 	
-	last_position = position
+	if movement.y > 0:
+		last_direction = "f"
+	elif movement.y < 0:
+		last_direction = "b"
+	elif movement.x > 0:
+		last_direction = "r"
+	elif movement.x < 0:
+		last_direction = "l"
