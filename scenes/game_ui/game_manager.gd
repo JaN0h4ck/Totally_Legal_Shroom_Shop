70f04b3053bbc54da4746435196aaf99bf428d4a
JaN0h4ck <git@jan-nothacker.de>
2025-07-30 09:20:21 +0200

    make game pausable

--- scenes/game_ui/game_manager.gd
+++ scenes/game_ui/game_manager.gd
@@ -3,6 +3,19 @@ extends Node
 
 @onready var ui_layer: Control = $"../UILayer"
 @export var interact_scene: PackedScene
+@export var pause_scene: PackedScene
+
+var is_paused: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action("ui_cancel"):
+		if not is_paused:
+			is_paused = true
+			var instance: PauseMenu = pause_scene.instantiate()
+			get_parent().add_child(instance)
+			get_viewport().set_input_as_handled()
+		else:
+			is_paused = false
 
 func show_interact_prompt(prompt: String):
 	var instance: InputPrompt = interact_scene.instantiate()
