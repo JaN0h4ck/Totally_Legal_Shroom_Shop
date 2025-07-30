e25547feec93cee8a541d75c6cc279360553e815
JaN0h4ck <git@jan-nothacker.de>
2025-07-30 13:45:44 +0200

    skippable dialog

--- scenes/game_ui/dialog_system/dialog_panel.gd
+++ scenes/game_ui/dialog_system/dialog_panel.gd
@@ -3,6 +3,7 @@ extends Panel
 
 @onready var label: Label = $MarginContainer/Label
 @export var speed_char_ps: float = 32.0
+var tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,12 +11,20 @@ func _ready() -> void:
 		push_error("Label component missing in Dialog Panel")
 		queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if not event.is_action_pressed("skip_dialogue"):
+		return
+	if tween and tween.is_valid(): 
+		tween.kill()
+		label.visible_characters = -1
+		get_viewport().set_input_as_handled()
+	else: 
+		get_viewport().set_input_as_handled()
+		queue_free()
 
 func _start_dialog(text: String):
 	label.text = text
 	var seconds: float = float(text.length()) / speed_char_ps
-	var tween = get_tree().create_tween()
+	if tween and tween.is_valid(): tween.kill()
+	tween = get_tree().create_tween()
 	tween.tween_property(label, "visible_characters", text.length(), seconds).from(0)
