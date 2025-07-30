6c8afa4cfddf747060ff24fbf58c290508f4d3ee
MurtaghDorn <vogelmarkus03@gmail.com>
2025-07-30 11:40:48 +0200

    Added Interactibles to Level

---
+++ scenes/game_scene/store_room.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+func _interact_mushroom_theke():
+	pass # Replace with function body.
+
+
+func _interact_customer():
+	pass # Replace with function body.
+
+
+func _interact_register():
+	pass # Replace with function body.
+
+
+func _interact_lexikon():
+	pass # Replace with function body.
+
+
+func _interact_lever():
+	pass # Replace with function body.
+
+
+func _interact_certificate():
+	pass # Replace with function body.
