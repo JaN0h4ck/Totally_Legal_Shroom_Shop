46457b1b342b02522980c7ba69e1601faab5c60f
JaN0h4ck <git@jan-nothacker.de>
2025-07-30 11:39:36 +0200

    adjust player movement

--- Character/player.gd
+++ Character/player.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 4000
 
-func getInput(): 
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
+func getInput(delta: float): 
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	velocity = input_direction * speed * delta
 
-func _physics_process(delta):
-	getInput()
+func _physics_process(delta: float):
+	getInput(delta)
 	move_and_slide()
