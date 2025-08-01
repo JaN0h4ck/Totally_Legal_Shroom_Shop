ceeb91a0c41402a29326e23fd5f958714ba2908a
Markus Vogel <murtaghdorn@proton.me>
2025-08-01 10:04:10 +0200

    Changed signal npc_dropped tipe to enum and added signal description

--- scripts/event_bus.gd
+++ scripts/event_bus.gd
@@ -1,23 +1,38 @@
 extends Node
 
+## Ausgelöst wenn mit Kunden geredet wird
 signal interact_customer
+## Ausgelöst wenn mit Kasse interagiert wird
 signal interact_register
+## Ausgelöst wenn mit Lexikon interagiert wird
 signal interact_lexikon
+## Ausgelöst wenn mit Hebel interagiert wird
 signal interact_lever
+## Ausgelöst wenn mit Zertifikat interagiert wird
 signal interact_certificate
+## Ausgelöst wenn mit Tür richtung Keller interagiert wird
 signal interact_basement
+## Ausgelöst wenn mit Tür richtung Shop interagiert wird
 signal interact_shop
 
+## Ausgelöst wenn ein NPC auf die Falltür tritt
 signal npc_entered_trapdoor
+## Ausgelöst wenn ein NPC die Falltür verlässt
 signal npc_left_trapdoor
+## Ausgelöst wenn ein NPC den Shop verlässt (entweder durch Falltür oder als Kunde)
 signal npc_left_shop
-signal npc_dropped(name : String)
+## Ausgelöst wenn ein NPC durch die Falltür fällt, NPC Enum Name wird mit übergeben
+signal npc_dropped(npc_name : base_npc.npc_name_enum)
 
+## Ausgelöst wenn ein Dialog gestartet wird
 signal dialog_started
+## Ausgelöst wenn ein Dialog endet
 signal dialog_ended
 
+## Ausgelöst wenn Inventar geöffnet werden soll
 signal open_inventory
 
+## Ausgelöst wenn Lexikon geschlossen wird
 signal lexicon_back
 
 func _on_interact_customer():
@@ -34,3 +49,33 @@ func _on_interact_lever():
 
 func _on_interact_certificate():
 	interact_certificate.emit()
+
+func _on_interact_basement():
+	interact_basement.emit()
+
+func _on_interact_shop():
+	interact_shop.emit()
+
+func _on_npc_entered_trapdoor():
+	npc_entered_trapdoor.emit()
+
+func _on_npc_left_trapdoor():
+	npc_left_trapdoor.emit()
+
+func _on_npc_left_shop():
+	npc_left_shop.emit()
+
+func _on_npc_dropped(npc_name : base_npc.npc_name_enum):
+	npc_dropped.emit(npc_name)
+
+func _on_dialog_started():
+	dialog_started.emit()
+
+func _on_dialog_ended():
+	dialog_ended.emit()
+
+func _on_open_inventory():
+	open_inventory.emit()
+
+func _on_lexikon_closed():
+	lexicon_back.emit()
