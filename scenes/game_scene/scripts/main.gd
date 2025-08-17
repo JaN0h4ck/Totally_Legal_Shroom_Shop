3aa508d7a63c852196605ef9d921adc52ffaf95c
JaN0h4ck <git@jan-nothacker.de>
2025-08-17 10:05:19 +0200

    play open and close sound again

--- scenes/game_scene/scripts/main.gd
+++ scenes/game_scene/scripts/main.gd
@@ -1,35 +1,10 @@
 extends Node2D
 
-@export var basement_position : Node2D
-@export var shop_position : Node2D
 
-@export var shop_camera : Camera2D
-@export var basement_camera : Camera2D
-
-@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
-
-func _ready():
-	EventBus.interact_basement.connect(teleport_basement)
-	EventBus.interact_shop.connect(teleport_shop)
 
 func teleport_basement():
-	audio_stream_player.set("parameters/switch_to_clip", &"Basement")
 	$teleport_point_basement/BasementDoorAudio.play()
-	var player : Player = get_tree().get_first_node_in_group("player")
-	player.position = basement_position.position
-	
-	shop_camera.enabled = false
-	basement_camera.enabled = true
 
 func teleport_shop():
-	audio_stream_player.set("parameters/switch_to_clip", &"Shop")
 	$teleport_point_shopt/ShopDoorAudio.play()
 	var player : Player = get_tree().get_first_node_in_group("player")
-	player.position = shop_position.position
-	
-	basement_camera.enabled = false
-	shop_camera.enabled = true
-
-func cameraShop():
-	shop_camera.enabled = true
-	basement_camera.enabled = false
--- scenes/game_scene/scripts/main_subviewports.gd
+++ scenes/game_scene/scripts/main_subviewports.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var shop: SubViewportContainer = $Shop
 @onready var basement: SubViewportContainer = $Basement
+@onready var door_sound: AudioStreamPlayer = $DoorSound
 
 
 func _ready():
@@ -13,11 +14,13 @@ func _teleport_basement():
 	audio_stream_player.set("parameters/switch_to_clip", &"Basement")
 	shop.visible = false
 	basement.visible = true
+	door_sound.play()
 
 func _teleport_shop():
 	audio_stream_player.set("parameters/switch_to_clip", &"Shop")
 	basement.visible = false
 	shop.visible = true
+	door_sound.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
