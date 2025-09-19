92a1d3fa5fb9429ad89f85b7ab61abb815cb86f6
Markus <murtaghdorn@proton.me>
2025-09-19 09:58:57 +0200

    Expanded Dungeon Inventory to fit more mushrooms

---
+++ inventory/inventory_dungeon_ui.gd
@@ -0,0 +1,9 @@
+extends Control
+# Close Inventory
+
+func _ready() -> void:
+	get_tree().paused = true
+func _process(_delta: float) -> void:
+	if(Input.is_action_pressed("ui_cancel")):
+		get_tree().paused = false
+		self.queue_free()
