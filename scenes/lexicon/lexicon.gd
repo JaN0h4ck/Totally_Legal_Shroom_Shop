ee59c6363fb0096f1d4165490556623261589baf
Jan Nothacker <git@jan-nothacker.de>
2025-09-23 23:03:23 +0200

    book sounds

--- scenes/lexicon/lexicon.gd
+++ scenes/lexicon/lexicon.gd
@@ -6,6 +6,7 @@ const LAST_PAGE: int = 8
 @onready var anim = $AnimatedSprite2D
 @export var count: int = 0
 @onready var sub_viewport_container: SubViewportContainer = $"../.."
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready():
 	anim.play("Page1")
@@ -13,14 +14,18 @@ func _ready():
 
 func _process(_delta: float) -> void:
 	if(Input.is_action_just_pressed("ui_left")):
+		var old = count
 		count -= 1
 		count = clampi(count, FIRST_PAGE, LAST_PAGE)
 		page()
+		play_audio(old)
 		return
 	if(Input.is_action_just_pressed("ui_right")):
+		var old = count
 		count += 1
 		count = clampi(count, FIRST_PAGE, LAST_PAGE)
 		page()
+		play_audio(old)
 		return
 	if(Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("joy_cancel")):
 		get_tree().paused = false
@@ -37,3 +42,7 @@ func page():
 		6: anim.play("Page7")
 		7: anim.play("Page8")
 		8: anim.play("Page9")
+
+func play_audio(old: int):
+	if count == old: return
+	audio_stream_player.play()
