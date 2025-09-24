7e7b840689f9dbf36dd1bc5bd16a879509e7d8d0
Markus <murtaghdorn@proton.me>
2025-09-24 13:19:49 +0200

    Applyed Font

--- scenes/journal/journal_ui_item.gd
+++ scenes/journal/journal_ui_item.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var icon: Sprite2D = $HBoxContainer/Icon
+@onready var npc_icon: Sprite2D = $HBoxContainer/NPCIcon
 @onready var text: Label = $HBoxContainer/Text
 
 func _ready() -> void:
@@ -12,22 +12,22 @@ func update_text(number : int):
 func update_icon_to_killed(killed_npc : GLOBALS.npc_names):
 	match killed_npc:
 		GLOBALS.npc_names.alien:
-			icon.texture = load("res://assets/character/alien/alien_portrait.png")
+			npc_icon.texture = load("res://assets/character/alien/alien_portrait.png")
 		GLOBALS.npc_names.angry:
-			icon.texture = load("res://assets/character/angry/angry_portrait.png")
+			npc_icon.texture = load("res://assets/character/angry/angry_portrait.png")
 		GLOBALS.npc_names.beff:
-			icon.texture = load("res://assets/character/beff/beff_portrait.png")
+			npc_icon.texture = load("res://assets/character/beff/beff_portrait.png")
 		GLOBALS.npc_names.celeb:
-			icon.texture = load("res://assets/character/celebrity/celeb_portrait.png")
+			npc_icon.texture = load("res://assets/character/celebrity/celeb_portrait.png")
 		GLOBALS.npc_names.conspiracy:
-			icon.texture = load("res://assets/character/conspiracy/conspiracy_portrait.png")
+			npc_icon.texture = load("res://assets/character/conspiracy/conspiracy_portrait.png")
 		GLOBALS.npc_names.cook:
-			icon.texture = load("res://assets/character/cook/cook_portrait.png")
+			npc_icon.texture = load("res://assets/character/cook/cook_portrait.png")
 		GLOBALS.npc_names.cowboy:
-			icon.texture = load("res://assets/character/cowboy/cowboy_portrait.png")
+			npc_icon.texture = load("res://assets/character/cowboy/cowboy_portrait.png")
 		GLOBALS.npc_names.enthusiast:
-			icon.texture = load("res://assets/character/enthusiast/enthusiast_portrait.png")
+			npc_icon.texture = load("res://assets/character/enthusiast/enthusiast_portrait.png")
 		GLOBALS.npc_names.nerd:
-			icon.texture = load("res://assets/character/nerd/nerd_portrait.png")
+			npc_icon.texture = load("res://assets/character/nerd/nerd_portrait.png")
 		GLOBALS.npc_names.opa:
-			icon.texture = load("res://assets/character/grandpa/grandpa_portrait.png")
+			npc_icon.texture = load("res://assets/character/grandpa/grandpa_portrait.png")
