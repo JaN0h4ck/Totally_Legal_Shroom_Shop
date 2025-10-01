d48763e82d7c800400a9c63f28844a9b9b9c1398
Markus <murtaghdorn@proton.me>
2025-10-01 10:19:00 +0200

    Removed unused inventory code

--- inventory/inventory_dungeon_ui.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-# Close Inventory
-
-func _ready() -> void:
-	get_tree().paused = true
-func _process(_delta: float) -> void:
-	if(Input.is_action_pressed("ui_cancel") or Input.is_action_pressed("joy_cancel")):
-		get_tree().paused = false
-		self.queue_free()
--- scenes/game_ui/game_manager.gd
+++ scenes/game_ui/game_manager.gd
@@ -4,34 +4,28 @@ extends Node
 @onready var ui_layer: CanvasLayer = $"../UILayer"
 @export var interact_scene: PackedScene
 @export var pause_scene: PackedScene
-@export var shop_inv_scene: PackedScene
-@export var dungeon_inv_scene: PackedScene
+@export var inventory_scene: PackedScene
 @export var lexicon_scene: PackedScene
 @export var journal_scene: PackedScene
 
 var is_paused: bool = false
 
 func _ready() -> void:
-	EventBus.open_inventory.connect(_on_open_shop_inventory)
-	EventBus.open_dungeon_inventory.connect(_on_open_dungeon_inventory)
+	EventBus.open_inventory.connect(_on_open_inventory)
+	EventBus.open_dungeon_inventory.connect(_on_open_inventory)
 	EventBus.open_inventory_short.connect(_on_open_inventory_short)
 	EventBus.interact_lexikon.connect(_on_open_lexikon)
 	EventBus.interact_journal.connect(_on_open_journal)
 
-func _on_open_shop_inventory():
-	var instance = shop_inv_scene.instantiate()
+func _on_open_inventory():
+	var instance = inventory_scene.instantiate()
 	ui_layer.add_child(instance)
 	get_tree().paused = true
 
 func _on_open_inventory_short():
-	var instance = shop_inv_scene.instantiate()
+	var instance = inventory_scene.instantiate()
 	ui_layer.add_child(instance)
 	instance.close_inventory()
-
-func _on_open_dungeon_inventory():
-	var instance = dungeon_inv_scene.instantiate()
-	ui_layer.add_child(instance)
-	get_tree().paused = true
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
